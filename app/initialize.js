7c005b45baf18fa94c37c8015b38925483f98469
mgarbacz <mich.garbacz@gmail.com>
2014-02-24 03:48:45 -0600

    Add feature to allow adding of strings to Trie
    * Add button appears when no string is matched via Trie's `find` method
    and an input is present

--- app/initialize.js
+++ app/initialize.js
@@ -7,19 +7,50 @@ trie.add('bad');
 
 var searchInput = document.getElementById('search-input');
 var searchResults = document.getElementById('search-results');
+var addButton = document.getElementById('add-button');
+
+addButton.style.display = 'none';
+addButton.disabled = true;
 
 searchInput.oninput = function() {
   searchResults.innerHTML = '';
 
   var results = trie.search(this.value);
+  var exists = trie.find(this.value);
 
-  for (var result in results) {
-    var el = document.createElement('li');
-    var elText = document.createTextNode(results[result]);
-    el.appendChild(elText);
-    searchResults.appendChild(el);
+  if (results) {
+    for (var result in results) {
+      var el = document.createElement('li');
+      var elText = document.createTextNode(results[result]);
+      el.appendChild(elText);
+      searchResults.appendChild(el);
+    }
   }
 
+  if (exists || !this.value) {
+    addButton.style.display = 'none';
+    addButton.disabled = true;
+  } else {
+    addButton.style.display = 'inline-block';
+    addButton.disabled = false;
+  }
+};
+
+searchInput.onkeyup = function(event) {
+  if (event.which === 13) {
+    addButton.click();
+  }
+};
+
+addButton.onclick = function() {
+  trie.add(searchInput.value);
+
+  var result = trie.find(searchInput.value);
+
+  if (result) {
+    searchInput.oninput();
+    alert(result + ' was added!');
+  }
 };
 
 console.log(trie);
