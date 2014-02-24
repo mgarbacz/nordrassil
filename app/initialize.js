8c9c6a0786496204448e57180303cc31c479d056
mgarbacz <mich.garbacz@gmail.com>
2014-02-24 03:19:16 -0600

    Add search functionality to UI

--- app/initialize.js
+++ app/initialize.js
@@ -5,4 +5,21 @@ trie = new Trie();
 trie.add('bat');
 trie.add('bad');
 
+var searchInput = document.getElementById('search-input');
+var searchResults = document.getElementById('search-results');
+
+searchInput.oninput = function() {
+  searchResults.innerHTML = '';
+
+  var results = trie.search(this.value);
+
+  for (var result in results) {
+    var el = document.createElement('li');
+    var elText = document.createTextNode(results[result]);
+    el.appendChild(elText);
+    searchResults.appendChild(el);
+  }
+
+};
+
 console.log(trie);
