af4b4d10416346e5f9d263b6a7ebb9fb15d955d2
mgarbacz <mich.garbacz@gmail.com>
2014-03-01 13:48:23 -0600

    Add tests for Trie add, search, and find methods

--- test/trie.js
+++ test/trie.js
@@ -10,4 +10,16 @@ describe('Trie', function() {
   it('should be an object', function() {
     expect(trie).to.be.ok;
   });
+
+  it('should have add method', function() {
+    expect(trie.add).to.be.an.instanceof(Function);
+  });
+
+  it('should have search method', function() {
+    expect(trie.search).to.be.an.instanceof(Function);
+  });
+
+  it('should have find method', function() {
+    expect(trie.find).to.be.an.instanceof(Function);
+  });
 });
