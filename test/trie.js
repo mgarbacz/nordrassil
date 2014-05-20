b80537b242f30be45da0098186dfa50814c49eaf
mgarbacz <mich.garbacz@gmail.com>
2014-05-19 17:30:10 -0700

    Reorganize test cases under describe namespaces

--- test/trie.js
+++ test/trie.js
@@ -15,76 +15,82 @@ describe('Trie', function() {
     expect(trie.root).to.be.ok;
   });
 
-  it('should have add method', function() {
-    expect(trie.add).to.be.an.instanceof(Function);
+  describe('add method', function() {
+    it('should be a function', function() {
+      expect(trie.add).to.be.an.instanceof(Function);
+    });
+
+    it('should return itself', function() {
+      expect(trie.add('test')).to.equal(trie);
+    });
+
+    it('should trim leading/trailing spaces', function() {
+      expect(trie.add('  test   ').root.children['t']).to.be.ok;
+    });
   });
 
-  it('should have search method', function() {
-    expect(trie.search).to.be.an.instanceof(Function);
+  describe('search method', function() {
+    it('should be a function', function() {
+      expect(trie.search).to.be.an.instanceof(Function);
+    });
+
+    it('should return an array', function() {
+      expect(trie.search('test')).to.be.an.instanceof(Array);
+    });
+
+    it('should return an empty array when there is no match', function() {
+      expect(trie.search('test')).to.be.empty;
+    });
+
+    it('should return a single element array when there is one match', function() {
+      expect(trie.add('test').search('test')).to.have.length(1);
+    });
+
+    it('should return all matching strings', function() {
+      trie.add('test');
+      trie.add('tester');
+      trie.add('nottest');
+
+      var result = trie.search('test');
+
+      expect(result).to.contain('test');
+      expect(result).to.contain('tester');
+      expect(result).to.not.contain('nottest');
+    });
+
+    it('should ignore case', function() {
+      trie.add('test lower');
+      trie.add('TEST UPPER');
+      trie.add('TeSt MiXeD');
+
+      var result = trie.search('test');
+
+      expect(result).to.contain('test lower');
+      expect(result).to.contain('TEST UPPER');
+      expect(result).to.contain('TeSt MiXeD');
+    });
   });
 
-  it('should have find method', function() {
-    expect(trie.find).to.be.an.instanceof(Function);
-  });
+  describe('find method', function() {
+    it('should be a function', function() {
+      expect(trie.find).to.be.an.instanceof(Function);
+    });
 
-  it('should return itself when adding', function() {
-    expect(trie.add('test')).to.equal(trie);
-  });
+    it('should return true when found', function() {
+      expect(trie.add('test').find('test')).to.be.true;
+    });
 
-  it('should return true when found', function() {
-    expect(trie.add('test').find('test')).to.be.true;
-  });
+    it('should return false when not found', function() {
+      expect(trie.find('test')).to.be.false;
+    });
 
-  it('should return false when not found', function() {
-    expect(trie.find('test')).to.be.false;
-  });
+    it('should consider case', function() {
+      trie.add('TeSt');
 
-  it('should consider case when finding', function() {
-    trie.add('TeSt');
-
-    expect(trie.find('test')).to.be.false;
-    expect(trie.find('TEST')).to.be.false;
-    expect(trie.find('TeSt')).to.be.true;
-  });
-
-  it('should trim leading/trailing spaces when adding', function() {
-    expect(trie.add('  test   ').find('test')).to.be.true;
-  });
-
-  it('should return an array when searching', function() {
-    expect(trie.search('test')).to.be.an.instanceof(Array);
-  });
-
-  it('should return an empty array when searching with no match', function() {
-    expect(trie.search('test')).to.be.empty;
-  });
-
-  it('should return a single element array when searching with one match', function() {
-    expect(trie.add('test').search('test')).to.have.length(1);
-  });
-
-  it('should return all matching strings when searching', function() {
-    trie.add('test');
-    trie.add('tester');
-    trie.add('nottest');
-
-    var result = trie.search('test');
-
-    expect(result).to.contain('test');
-    expect(result).to.contain('tester');
-    expect(result).to.not.contain('nottest');
-  });
-
-  it('should ignore case when searching', function() {
-    trie.add('test lower');
-    trie.add('TEST UPPER');
-    trie.add('TeSt MiXeD');
-
-    var result = trie.search('test');
-
-    expect(result).to.contain('test lower');
-    expect(result).to.contain('TEST UPPER');
-    expect(result).to.contain('TeSt MiXeD');
+      expect(trie.find('test')).to.be.false;
+      expect(trie.find('TEST')).to.be.false;
+      expect(trie.find('TeSt')).to.be.true;
+    });
   });
 
 });
