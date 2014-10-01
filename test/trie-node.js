fcf96541d607e2b83e00a9e2b3e7807b1d1602bc
mgarbacz <mich.garbacz@gmail.com>
2014-09-30 23:19:20 -0500

    Use 4 spaces instead of 2 for test js

--- test/trie-node.js
+++ test/trie-node.js
@@ -1,59 +1,61 @@
 var TrieNode = require('../app/trie-node');
 
 describe('TrieNode', function() {
-  var trienode;
+    var trienode;
 
-  beforeEach(function() {
-    trienode = new TrieNode();
-  });
-
-  it('should be an object', function() {
-    expect(trienode).to.be.ok;
-  });
-
-  it('should set a character on construction', function() {
-    trienode = new TrieNode('t');
-
-    expect(trienode.character).to.equal('t');
-  });
-
-  it('should store an array of strings', function() {
-    expect(trienode.strings).to.be.an.instanceof(Array);
-  });
-
-  describe('add method', function() {
-    it('should be a function', function() {
-      expect(trienode.add).to.be.an.instanceof(Function);
+    beforeEach(function() {
+        trienode = new TrieNode();
     });
 
-    it('should return undefined', function() {
-      expect(trienode.add('test', 0)).to.be.undefined;
+    it('should be an object', function() {
+        expect(trienode).to.be.ok;
     });
 
-    it('should store next case insensitive character', function() {
-      trienode.add('tEsT', 3);
-      expect(trienode.children['t']).to.be.ok;
+    it('should set a character on construction', function() {
+        trienode = new TrieNode('t');
+
+        expect(trienode.character).to.equal('t');
     });
 
-    it('should store case sensitive string', function() {
-      trienode.add('tEsT', 3);
-      expect(trienode.children['t'].strings).to.contain('tEsT');
-    });
-  });
-
-  describe('search method', function() {
-    it('should be a function', function() {
-      expect(trienode.search).to.be.an.instanceof(Function);
+    it('should store an array of strings', function() {
+        expect(trienode.strings).to.be.an.instanceof(Array);
     });
 
-    it('should return an array', function() {
-      expect(trienode.search('test')).to.be.an.instanceof(Array);
-    });
-  });
+    describe('add method', function() {
+        it('should be a function', function() {
+            expect(trienode.add).to.be.an.instanceof(Function);
+        });
 
-  describe('find method', function() {
-    it('should be a function', function() {
-      expect(trienode.find).to.be.an.instanceof(Function);
+        it('should return undefined', function() {
+            expect(trienode.add('test', 0)).to.be.undefined;
+        });
+
+        it('should store next case insensitive character', function() {
+            trienode.add('tEsT', 3);
+
+            expect(trienode.children['t']).to.be.ok;
+        });
+
+        it('should store case sensitive string', function() {
+            trienode.add('tEsT', 3);
+
+            expect(trienode.children['t'].strings).to.contain('tEsT');
+        });
     });
-  });
+
+    describe('search method', function() {
+        it('should be a function', function() {
+            expect(trienode.search).to.be.an.instanceof(Function);
+        });
+
+        it('should return an array', function() {
+            expect(trienode.search('test')).to.be.an.instanceof(Array);
+        });
+    });
+
+    describe('find method', function() {
+        it('should be a function', function() {
+            expect(trienode.find).to.be.an.instanceof(Function);
+        });
+    });
 });
--- test/trie.js
+++ test/trie.js
@@ -1,96 +1,96 @@
 var Trie = require('../app/trie');
 
 describe('Trie', function() {
-  var trie;
+    var trie;
 
-  beforeEach(function() {
-    trie = new Trie();
-  });
-
-  it('should be an object', function() {
-    expect(trie).to.be.ok;
-  });
-
-  it('should have a root', function() {
-    expect(trie.root).to.be.ok;
-  });
-
-  describe('add method', function() {
-    it('should be a function', function() {
-      expect(trie.add).to.be.an.instanceof(Function);
+    beforeEach(function() {
+        trie = new Trie();
     });
 
-    it('should return itself', function() {
-      expect(trie.add('test')).to.equal(trie);
+    it('should be an object', function() {
+        expect(trie).to.be.ok;
     });
 
-    it('should trim leading/trailing spaces', function() {
-      expect(trie.add('  test   ').root.children['t']).to.be.ok;
-    });
-  });
-
-  describe('search method', function() {
-    it('should be a function', function() {
-      expect(trie.search).to.be.an.instanceof(Function);
+    it('should have a root', function() {
+        expect(trie.root).to.be.ok;
     });
 
-    it('should return an array', function() {
-      expect(trie.search('test')).to.be.an.instanceof(Array);
+    describe('add method', function() {
+        it('should be a function', function() {
+            expect(trie.add).to.be.an.instanceof(Function);
+        });
+
+        it('should return itself', function() {
+            expect(trie.add('test')).to.equal(trie);
+        });
+
+        it('should trim leading/trailing spaces', function() {
+            expect(trie.add('  test   ').root.children['t']).to.be.ok;
+        });
     });
 
-    it('should return an empty array when there is no match', function() {
-      expect(trie.search('test')).to.be.empty;
+    describe('search method', function() {
+        it('should be a function', function() {
+            expect(trie.search).to.be.an.instanceof(Function);
+        });
+
+        it('should return an array', function() {
+            expect(trie.search('test')).to.be.an.instanceof(Array);
+        });
+
+        it('should return an empty array when there is no match', function() {
+            expect(trie.search('test')).to.be.empty;
+        });
+
+        it('should return a single element array when there is one match', function() {
+            expect(trie.add('test').search('test')).to.have.length(1);
+        });
+
+        it('should return all matching strings', function() {
+            trie.add('test');
+            trie.add('tester');
+            trie.add('nottest');
+
+            var result = trie.search('test');
+
+            expect(result).to.contain('test');
+            expect(result).to.contain('tester');
+            expect(result).to.not.contain('nottest');
+        });
+
+        it('should ignore case', function() {
+            trie.add('test lower');
+            trie.add('TEST UPPER');
+            trie.add('TeSt MiXeD');
+
+            var result = trie.search('test');
+
+            expect(result).to.contain('test lower');
+            expect(result).to.contain('TEST UPPER');
+            expect(result).to.contain('TeSt MiXeD');
+        });
     });
 
-    it('should return a single element array when there is one match', function() {
-      expect(trie.add('test').search('test')).to.have.length(1);
+    describe('find method', function() {
+        it('should be a function', function() {
+            expect(trie.find).to.be.an.instanceof(Function);
+        });
+
+        it('should return true when found', function() {
+            expect(trie.add('test').find('test')).to.be.true;
+        });
+
+        it('should return false when not found', function() {
+            expect(trie.find('test')).to.be.false;
+        });
+
+        it('should consider case', function() {
+            trie.add('TeSt');
+
+            expect(trie.find('test')).to.be.false;
+            expect(trie.find('TEST')).to.be.false;
+            expect(trie.find('TeSt')).to.be.true;
+        });
     });
-
-    it('should return all matching strings', function() {
-      trie.add('test');
-      trie.add('tester');
-      trie.add('nottest');
-
-      var result = trie.search('test');
-
-      expect(result).to.contain('test');
-      expect(result).to.contain('tester');
-      expect(result).to.not.contain('nottest');
-    });
-
-    it('should ignore case', function() {
-      trie.add('test lower');
-      trie.add('TEST UPPER');
-      trie.add('TeSt MiXeD');
-
-      var result = trie.search('test');
-
-      expect(result).to.contain('test lower');
-      expect(result).to.contain('TEST UPPER');
-      expect(result).to.contain('TeSt MiXeD');
-    });
-  });
-
-  describe('find method', function() {
-    it('should be a function', function() {
-      expect(trie.find).to.be.an.instanceof(Function);
-    });
-
-    it('should return true when found', function() {
-      expect(trie.add('test').find('test')).to.be.true;
-    });
-
-    it('should return false when not found', function() {
-      expect(trie.find('test')).to.be.false;
-    });
-
-    it('should consider case', function() {
-      trie.add('TeSt');
-
-      expect(trie.find('test')).to.be.false;
-      expect(trie.find('TEST')).to.be.false;
-      expect(trie.find('TeSt')).to.be.true;
-    });
-  });
 
 });
