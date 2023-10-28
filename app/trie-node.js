f0794521d10b9c8f4835e34422266ea8d5e0cd9f
mgarbacz <michal@garbacz.io>
2023-10-27 22:40:12 -0400

    Modernize some html/js stuff to match vite patterns

--- app/trie-node.js
+++ app/trie-node.js
@@ -1,67 +1,61 @@
-var TrieNode = (function() {
-    function TrieNode(character) {
-        this.character = character || '';
-        this.children = {};
-        this.strings = [];
+export default function TrieNode(character) {
+  this.character = character || '';
+  this.children = {};
+  this.strings = [];
+}
+
+TrieNode.prototype.add = function(string, index) {
+  var currChar = string[index];
+
+  if (currChar) {
+    currChar = currChar.toLowerCase();
+    if (!this.children[currChar]) {
+      this.children[currChar] = new TrieNode(currChar);
     }
+    this.children[currChar].add(string, index + 1);
+  } else {
+    this.strings.push(string);
+  }
+};
 
-    TrieNode.prototype.add = function(string, index) {
-        var currChar = string[index];
+TrieNode.prototype.search = function(string, index) {
+  var currChar = string[index];
 
-        if (currChar) {
-            currChar = currChar.toLowerCase();
-            if (!this.children[currChar]) {
-                this.children[currChar] = new TrieNode(currChar);
-            }
-            this.children[currChar].add(string, index + 1);
-        } else {
-            this.strings.push(string);
-        }
-    };
+  if (currChar) {
+    currChar = currChar.toLowerCase();
+    if (!this.children[currChar]) {
+      return [];
+    } else {
+      return this.children[currChar].search(string, index + 1);
+    }
+  } else if (index === 0) {
+    return [];
+  } else {
+    return collectStrings(this);
+  }
+};
 
-    TrieNode.prototype.search = function(string, index) {
-        var currChar = string[index];
+var collectStrings = function(node) {
+  var strings = node.strings.slice();
 
-        if (currChar) {
-            currChar = currChar.toLowerCase();
-            if (!this.children[currChar]) {
-                return [];
-            } else {
-                return this.children[currChar].search(string, index + 1);
-            }
-        } else if (index === 0) {
-            return [];
-        } else {
-            return collectStrings(this);
-        }
-    };
+  for (var child in node.children) {
+    strings = strings.concat(collectStrings(node.children[child]));
+  }
 
-    var collectStrings = function(node) {
-        var strings = node.strings.slice();
+  return strings;
+};
 
-        for (var child in node.children) {
-            strings = strings.concat(collectStrings(node.children[child]));
-        }
+TrieNode.prototype.find = function(string, index) {
+  var currChar = string[index];
 
-        return strings;
-    };
-
-    TrieNode.prototype.find = function(string, index) {
-        var currChar = string[index];
-
-        if (currChar) {
-            currChar = currChar.toLowerCase();
-            if (!this.children[currChar]) {
-                return false;
-            } else {
-                return this.children[currChar].find(string, index + 1);
-            }
-        } else {
-            return this.strings.indexOf(string) > -1 ? true : false;
-        }
-    };
-
-    return TrieNode;
-})();
-
-module.exports = TrieNode;
+  if (currChar) {
+    currChar = currChar.toLowerCase();
+    if (!this.children[currChar]) {
+      return false;
+    } else {
+      return this.children[currChar].find(string, index + 1);
+    }
+  } else {
+    return this.strings.indexOf(string) > -1 ? true : false;
+  }
+};
